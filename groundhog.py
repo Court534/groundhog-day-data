5dd45777b769bb96b33360022726990647b7e1cd
Courtney Stow <courney-stow@hotmail.co.uk>
2022-10-25 20:00:37 +0100

    Removed a duplicate piece of code on line 57

--- groundhog.py
+++ groundhog.py
@@ -14,35 +14,35 @@ print(df.info())
 conn = sqlite3.connect("groundhog-day-data.db")
 cur = conn.cursor()
 
-cur.execute('''
-          CREATE TABLE groundhogdata
-          ([year] INTEGER PRIMARY KEY, [punxsutawney_phil] TEXT, [feb_average_temp] TEXT, [march_average_temp] TEXT)
-          ''')
+# cur.execute('''
+#           CREATE TABLE groundhogdata
+#           ([year] INTEGER PRIMARY KEY, [punxsutawney_phil] TEXT, [feb_average_temp] TEXT, [march_average_temp] TEXT)
+#           ''')
                      
-conn.commit()
+# conn.commit()
 
-cur.execute('''
-          INSERT INTO groundhogdata (year, punxsutawney_phil, feb_average_temp, march_average_temp)
+# cur.execute('''
+#           INSERT INTO groundhogdata (year, punxsutawney_phil, feb_average_temp, march_average_temp)
 
-                VALUES
-                (2001,'Full Shadow', '33.98', '41.49'),
-                (2002,'Full Shadow', '36.39', '39.54'),
-                (2003,'Full Shadow', '32.79', '43.03'),
-                (2004,'Full Shadow', '33.57', '47.41'),
-                (2005,'Full Shadow', '37.94', '42.31'),
-                (2006,'Full Shadow', '34.83', '42.62'),
-                (2007,'No Shadow', '32.41', '47.66'),
-                (2008,'Full Shadow', '34.07', '41.86'),
-                (2009,'Full Shadow', '36.77', '42.87'),
-                (2010,'Full Shadow', '31.80', '43.57'),
-                (2011,'No Shadow', '33.04', '43.07'),
-                (2012,'Full Shadow', '37.51', '50.41'),
-                (2013,'No Shadow', '34.77', '40.91'),
-                (2014,'Full Shadow', '32.13', '40.51'),
-                (2015,'Full Shadow', '32.99', '45.39'),
-                (2016,'No Shadow', '39.47', '47.05')
-          ''')
-conn.commit()
+#                 VALUES
+#                 (2001,'Full Shadow', '33.98', '41.49'),
+#                 (2002,'Full Shadow', '36.39', '39.54'),
+#                 (2003,'Full Shadow', '32.79', '43.03'),
+#                 (2004,'Full Shadow', '33.57', '47.41'),
+#                 (2005,'Full Shadow', '37.94', '42.31'),
+#                 (2006,'Full Shadow', '34.83', '42.62'),
+#                 (2007,'No Shadow', '32.41', '47.66'),
+#                 (2008,'Full Shadow', '34.07', '41.86'),
+#                 (2009,'Full Shadow', '36.77', '42.87'),
+#                 (2010,'Full Shadow', '31.80', '43.57'),
+#                 (2011,'No Shadow', '33.04', '43.07'),
+#                 (2012,'Full Shadow', '37.51', '50.41'),
+#                 (2013,'No Shadow', '34.77', '40.91'),
+#                 (2014,'Full Shadow', '32.13', '40.51'),
+#                 (2015,'Full Shadow', '32.99', '45.39'),
+#                 (2016,'No Shadow', '39.47', '47.05')
+#           ''')
+# conn.commit()
 
 cur.execute('''
           SELECT
@@ -54,7 +54,6 @@ cur.execute('''
           ''')
 
 df = pd.DataFrame(cur.fetchall(), columns=['year','punxsutawney_phil', 'feb_average_temp', 'march_average_temp'])
-print (df)
 conn.commit()
 
 ## Accessing the information from the database with pandas
